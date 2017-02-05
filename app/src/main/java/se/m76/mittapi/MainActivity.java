747bb95421836282e2ad94623396b4b67f8e8c45
Jakob <jakoblindstrom@gmail.com>
2017-02-05 22:41:12 +0100

    LIte mer maps funkar

--- app/src/main/java/se/m76/mittapi/MainActivity.java
+++ app/src/main/java/se/m76/mittapi/MainActivity.java
@@ -1,12 +1,25 @@
 package se.m76.mittapi;
 
+import android.*;
+import android.Manifest;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import com.google.android.gms.location.LocationRequest;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,28 +28,42 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import se.m76.mittapi.models.Stars;
+
 import com.google.android.gms.maps.GoogleMap;
 
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements
+        OnMapReadyCallback     {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private GoogleMap googleMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG,"Hej hopp nu kör vi");
+        Log.i(TAG, "Hej hopp nu kör vi");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "Startar ...");
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        try {
+            // Loading map
+            initilizeMap();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
 
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
@@ -55,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onResponse(Call<Stars> call, Response<Stars> response) {
                 int statusCode = response.code();
-                Log.i(TAG,"Fick data?");
+                Log.i(TAG, "Fick data?");
                 Log.i(TAG, String.valueOf(response.body()));
                 Log.i(TAG, " " + response.body().getResult().size());
                 Log.i(TAG, "Namn:   " + response.body().getResult().get(0).getName());
@@ -63,14 +90,67 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public void onFailure(Call<Stars> call, Throwable t) {
-                Log.e(TAG,"Gick inte det!!",t);
+                Log.e(TAG, "Gick inte det!!", t);
             }
         });
 
     }
-    @Override
-        public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
+
+    /**
+     * function to load map. If map is not created it will create it for you
+     */
+    private void initilizeMap() {
+
+        MapFragment mapFrag = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        } else {
+            Toast.makeText(this, " Permission problem! ", Toast.LENGTH_LONG).show();
+        }
+
+     /*   mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //MapDemoActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+
 }
 
